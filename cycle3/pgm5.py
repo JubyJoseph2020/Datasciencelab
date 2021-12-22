838d65f6952f1655543355d45628aa98a3dadf4f
JubyJoseph2020 <74653069+JubyJoseph2020@users.noreply.github.com>
2021-12-22 12:00:58 +0530

    Add files via upload

---
+++ cycle3/pgm5.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 22 11:49:03 2021
+
+@author: sjcet
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+plt.title("Students transportation")
+plt.xlabel("Mode of Transport")
+plt.ylabel("Frequency")
+
+
+
+x = np.array(["walking","cycling","car","bus","train"])
+y = np.array([29,15,35,18,3])
+
+plt.bar(x, y, color = "#4CAF50",width = 0.1)
+plt.show()
